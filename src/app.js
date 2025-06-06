e6cb3419ea58fb5bf7cc7a06a61b92e57f136a49
Juan Betancur <juanfelipebetancurcano@gmail.com>
2025-06-06 11:24:08 -0500

    class 3

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+  console.log(path.join(__dirname, '..', 'public', 'index.html'));
 
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
