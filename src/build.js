92dd05baeaaa642360e925080b4a27196322e530
Juan Betancur <juanfelipebetancurcano@gmail.com>
2025-06-06 11:30:59 -0500

    class 3

--- src/build.js
+++ src/build.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const app = require('./app.js');
+const app = require('./app');
 const request = require('supertest');
 
 (async () => {
   const res = await request(app).get('/');
   fs.mkdirSync('public', { recursive: true });
   fs.writeFileSync('public/index.html', res.text);
-  console.log('Static HTML generated');
+  console.log('Static HTML generated!');
 })();
--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
-const app = require('../src/app.js');
+const app = require('../src/app');
 
 describe('GET /', () => {
-  it('Should return HTML', async () => {
+  it('should return HTML', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain('GitHub Actions');
+    expect(res.text).toContain('Bootcamp de GitHub Action en Código Facilito');
   });
 });
